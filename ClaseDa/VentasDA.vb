fb0b86fca4ceab0a201ae9514a612873d4113b53
Ernesto Mangeaud <example.ejemplar@gmail.com>
2019-05-28 22:47:11 -0300

    ventas archivos

---
+++ ClaseDa/VentasDA.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.SqlClient
+Imports ClaseNe
+Public Class VentasDA
+	Private db As New SqlConnection
+	Private com As New SqlCommand
+	Private da As SqlDataAdapter
+	Private ds1 As DataSet
+
+	Public Sub New()
+		Dim objcon As New ConexionDA
+		db = objcon.Abrir
+		com.Connection = db
+	End Sub
+
+
+End Class
---
+++ ClaseLn/VentasLN.vb
@@ -0,0 +1,10 @@
+﻿Imports ClaseDa
+Imports ClaseNe
+Public Class VentasLN
+	Private ventasDA As VentasDA
+
+
+	Public Sub New()
+		ventasDA = New VentasDA
+	End Sub
+End Class
