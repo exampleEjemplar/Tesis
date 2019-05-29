e9773b1bef430b792a18300298e30decf8fc99a3
Pedro <pedrorey31@gmail.com>
2019-05-28 23:40:21 -0300

    Creacion movimiento stock

---
+++ ClaseNe/MovimientoStockNE.vb
@@ -0,0 +1,45 @@
+﻿Public Class MovimientoStockNE
+    Private _Id As Integer
+
+    Public Property Id As Integer
+        Get
+            Return _Id
+        End Get
+        Set(ByVal value As Integer)
+            _Id = CStr(value)
+        End Set
+    End Property
+
+    Private _Cantidad As Integer
+
+    Public Property Cantidad As Double
+        Get
+            Return _Cantidad
+        End Get
+        Set(ByVal value As Double)
+            _Cantidad = CStr(value)
+        End Set
+    End Property
+
+    Private _ProductoId As Integer
+    Public Property ProductoId As Double
+        Get
+            Return _ProductoId
+        End Get
+        Set(ByVal value As Double)
+            _ProductoId = CStr(value)
+        End Set
+    End Property
+
+    Private _Fecha As DateTime
+
+    Public Property Fecha As DateTime
+        Get
+            Return _Fecha
+        End Get
+        Set(ByVal value As DateTime)
+            _Fecha = CStr(value)
+        End Set
+    End Property
+
+End Class
