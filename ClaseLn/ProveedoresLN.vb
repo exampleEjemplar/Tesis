0c815ca90d3409f7c58e9c68135ceb180f4f21d7
Ernesto Mangeaud <example.ejemplar@gmail.com>
2019-06-13 07:56:59 -0300

    se agrega logica  de clientes en proveedores.

--- ClaseLn/ProveedoresLN.vb
+++ ClaseLn/ProveedoresLN.vb
@@ -2,32 +2,34 @@
 Imports ClaseNe
 Public Class ProveedoresLN
 
-    Private objProveedoresAD As ProveedoresDA
+	Private objProveedoresAD As ProveedoresDA
 
-    Public idProv As Integer
-    Public QPacientes As Integer
-    Public QPacientesMod As Integer
-    Public IdPaciente1 As Integer
+	Public idProv As Integer
+	Public QPacientes As Integer
+	Public QPacientesMod As Integer
+	Public contador As Integer
 
-    Public Sub New()
-        objProveedoresAD = New ProveedoresDA
-    End Sub
+	Public Sub New()
+		objProveedoresAD = New ProveedoresDA
+	End Sub
 
-    Public Function ConsultaModificacion(ByVal nroDoc As Integer)
-        Return objProveedoresAD.ConsultaModificacion(nroDoc)
-    End Function
+	Public Function ConsultaModificacion(ByVal Id As Integer)
+		Return objProveedoresAD.ConsultaModificacion(Id)
+	End Function
 
-    Public Function CargaGrillaProveedores(ByVal parametros As Dictionary(Of String, String)) As DataSet
-        Return objProveedoresAD.CargaGrillaProveedores(parametros)
-    End Function
+	Public Function CargaGrillaProveedores(ByVal parametros As Dictionary(Of String, String)) As DataSet
+		Return objProveedoresAD.CargaGrillaProveedores(parametros)
+	End Function
 
-    Public Sub GrabarProveedores(ByVal pro As ProveedoresNE)
-        objProveedoresAD.grabarProveedores(pro)
-    End Sub
+	Public Sub GrabarProveedores(ByVal pro As ProveedoresNE)
+		objProveedoresAD.GrabarProveedores(pro)
+	End Sub
 
-    Public Sub ActualizarProveedores(ByVal pro As ProveedoresNE)
-        objProveedoresAD.ActualizarProveedores(pro)
-    End Sub
-
+	Public Sub ActualizarProveedores(ByVal pro As ProveedoresNE)
+		objProveedoresAD.ActualizarProveedores(pro)
+	End Sub
+	Public Function CargaGrillaProveedoreslistado(ByVal fechadesde As String, ByVal fechahasta As String) As DataSet
+		Return objProveedoresAD.CargaGrillaProveedoreslistado(fechadesde, fechahasta)
+	End Function
 End Class
 
