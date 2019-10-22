8720697cbc210262d312e3d203ca19aea6b72a89
Martin Cabrera <cabriu@hotmail.com>
2019-10-22 19:01:58 -0300

    Borro dataset y vamos a trabajar sobre una una solo conexion a la base para los reportes

--- ClaseUi/JoyeriaCrisolDataSet.vb
+++
@@ -1,4 +0,0 @@
-﻿
-
-Partial Public Class JoyeriaCrisolDataSet
-End Class
---
+++ ClaseUi/dslistadocliente.vb
@@ -0,0 +1,8 @@
+﻿Partial Class dslistadocliente
+End Class
+
+Namespace ClaseUi.dslistadoclienteTableAdapters
+
+    Partial Public Class SP_ListadodeClientesPorfechaTableAdapter
+    End Class
+End Namespace
