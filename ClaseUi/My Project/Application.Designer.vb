f87b3f194953cdcb9a94e857d14608812b2d8bad
Martin Cabrera <cabriu@hotmail.com>
2019-11-30 16:17:24 -0300

    MDI principal con menu opciones estadisticas

--- ClaseUi/My Project/Application.Designer.vb
+++ ClaseUi/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ClaseUi.FrmGestionVentas
+            Me.MainForm = Global.ClaseUi.MDIPrincipal
         End Sub
     End Class
 End Namespace
